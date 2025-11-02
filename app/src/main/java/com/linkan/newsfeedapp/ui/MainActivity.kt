5dd4a59864c7daa07a1045b7fc2aa8883f2d4745
linkan <linkanchauhan610@gmail.com>
2025-11-02 13:05:31 +0530

    #Task/3 : Fragment container added

--- app/src/main/java/com/linkan/newsfeedapp/ui/MainActivity.kt
+++ app/src/main/java/com/linkan/newsfeedapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.linkan.newsfeedapp
+package com.linkan.newsfeedapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.linkan.newsfeedapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
